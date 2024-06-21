96355a6e69d8a4a871e92dc4fc28db2f9ffa699f
pearhyunjin <pearhyunjin@gmail.com>
2024-06-21 23:31:01 +0900

    add: Lv2 최댓값과 최솟값

---
+++ algorithmSwift/Programmers/Lv2_최/main.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  algorithmSwift
+//
+//  Created by 배현진 on 6/7/24.
+//
+
+import Foundation
+
+var s: String = "1 2 3 4"
+
+// @discardableResult
+func solution(_ s:String) -> String {
+    let splitString = s.split(separator: " ")
+
+    var store: [Int] = []
+
+    splitString.map { element in
+        store.append(Int(element)!)
+    }
+
+    let result = String(store.min()!) + " " + String(store.max()!)
+
+    return result
+}
+
+print(solution(s))
